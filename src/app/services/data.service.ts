f59271db1f7b6e0b57f0b9adc67d649c50714cb7
Dar-Na <70232815+Dar-Na@users.noreply.github.com>
2022-08-05 18:19:08 +0200

    Fixed calendar selector. Now you can click at the new cell and everything will be okay.
    Fix #13

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,17 +8,22 @@ import * as moment from "moment";
 
 export class DataService {
   public date: BehaviorSubject<any> = new BehaviorSubject(moment())
+  public dateOrg: BehaviorSubject<any> = new BehaviorSubject(moment())
 
   changeMonth(num: number) {
     const value = this.date.value.add(num, 'month')
     this.date.next(value)
+    this.dateOrg.next(value)
   }
 
-  changeDate(date: moment.Moment) {
-    const value = this.date.value.set({
+  changeDate(date: moment.Moment, isDisabled: boolean) {
+    const value = this.dateOrg.value.set({
       date: date.date(),
       month: date.month()
     })
-    this.date.next(value)
+    if (isDisabled) {
+      this.date.next(value)
+    }
+    this.dateOrg.next(value)
   }
 }
